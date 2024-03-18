9d4cdbf444981298fa02a9f77c8a9244688079c6
Niko2357 <niktolka16.1@gmail.com>
2024-03-18 10:32:36 +0100

    class Game logic with all methods start and loadUser

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class GameLogic {
+    protected ArrayList<User> users = new ArrayList<>();
+
+    public void start() throws Exception{
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Hello, let's play Tic Tac Toe!");
+            boolean rep = true;
+            while (rep) {
+                System.out.println("Did you play before and would you like to continue? (y, n)");
+                String answ = sc.next();
+                if (answ.toLowerCase().equals("y")) {
+                    System.out.println("What's your name?");
+                    String name = sc.next();
+                    loadUser(name);
+                    rep = false;
+                } else if (answ.toLowerCase().equals("n")) {
+                    System.out.println("What's your name?");
+                    String name = sc.next();
+                    User user = new User(name, 0, 0);
+                    rep = false;
+                } else {
+                    System.out.println("What? What do you mean?");
+                }
+            }
+            System.out.println("How many games would you like to play?");
+            int numberOfGames = sc.nextInt();
+        }catch(Exception e){
+            System.out.println("Your input was wrong.");
+        }
+    }
+
+    public User loadUser(String name){
+        return users.get(users.indexOf(name));
+    }
 }
